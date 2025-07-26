cfbba4743911b6e6899d2a80c9eb18145e2e3008
James McLaughlin <jamesnicholasmclaughlin@gmail.com>
2025-07-26 16:49:01 +0100

    feat: Added basic pages for beginning functionality

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <section className={'h-100 my-auto'}>
+            <h1>James McLaughlin</h1>
+        </section>
+    )
+}
+
+export default Home
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import Home from './Home.tsx'
+import NotFound from './NotFound.tsx'
+
+export { Home, NotFound }
